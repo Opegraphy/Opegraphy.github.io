4a40be37f02adc4ab3b681638e061a30ecb6f30b
Opegraphy <teddybearopeyemi@gmail.com>
2023-02-05 02:47:06 +0100

    recaptcha

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Contact form <yourmail@domain.com>';
-$sendTo = 'opeyemikazeemjimoh@gmail.com';
+$sendTo = 'your@email.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Submitted! Thank you, I will get back to you soon!';
